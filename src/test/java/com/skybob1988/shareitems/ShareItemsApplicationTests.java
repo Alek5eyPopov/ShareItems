7c808fdacd5ab93f2043a761bc98ac469a5e23cf
aleks <aleksej_popov_88@mail.ru>
2024-10-10 13:30:04 +0300

    First commit

---
+++ src/test/java/com/skybob1988/shareitems/ShareItemsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.skybob1988.shareitems;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShareItemsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
